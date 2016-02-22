48eb7db42681c7c37040838c4d14b7cae239be7d
moforw <moforw@gmail.com>
2016-02-22 03:24:59 +0100

    planning

--- src/qiwics/Main.java
+++ src/qiwics/Main.java
@@ -1,8 +1,37 @@
 package qiwics;
 
-//Tiling interface
-///Add Tile class, extend Composite
-///Up to 8 tiles
+//tiling interface
+///add Tile class, extend Composite
+////add label
+////add edit to get something to focus on
+///up to 8 tiles
+///inc cols first, then rows
+///use gridlayout
+///unified menubar
+////each tile has it's own menus/items
+///dim inactive tiles
+///ctrl|1-8 to switch tile
+
+//add account Tile
+///used to view/edit a single account
+///email, password, imap_host/port, smtp_host/port
+///add accounts/new account menu item
+
+//add Table/Column/Index/Record like qiwics/C++
+///db package
+///use concurrent sets & maps
+
+//add Account class
+
+//generate RSA keys
+///generate if not exists and store in settings table
+
+//Add contact class
+
+//add Contact Tile
+///used to view/edit a single contact
+///name, email, public key
+///add contacts/new contact menu item
 
 public class Main {
 	public static void main(final String[] args) {
