207add2734fd728be824b30abdb389beaf24b23d
moforw <moforw@gmail.com>
2016-02-22 05:23:43 +0100

    formatting

--- src/qiwics/Account.java
+++ src/qiwics/Account.java
@@ -9,15 +9,15 @@ public class Account extends Rec {
 		public final StringCol Email =    stringCol("email")
 			.get((a)    -> a.email)
 			.set((a, v) -> a.email = v);
-		public final StringCol Password = stringCol("password")
-			.get((a)    -> a.password)
-			.set((a, v) -> a.password = v);
 		public final StringCol ImapHost = stringCol("imapHost")
 			.get((a)    -> a.imapHost)
 			.set((a, v) -> a.imapHost = v);
 		public final IntCol ImapPort =    intCol(   "imapPort")
 			.get((a)    -> a.imapPort)
 			.set((a, v) -> a.imapPort = v);
+		public final StringCol Password = stringCol("password")
+			.get((a)    -> a.password)
+			.set((a, v) -> a.password = v);
 		public final StringCol SmtpHost = stringCol("smtpHost")
 			.get((a)    -> a.smtpHost)
 			.set((a, v) -> a.smtpHost = v);
@@ -42,9 +42,9 @@ public class Account extends Rec {
 	}
 	
 	public String email;
-	public String password;
 	public String imapHost;
 	public int imapPort;
+	public String password;
 	public String smtpHost;
 	public int smtpPort;
 }
